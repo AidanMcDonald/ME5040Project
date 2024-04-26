85e69d442b8bbe978626f2f525a1140b8b54a023
Aidan Hunt McDonald <amcdona8@uwyo.edu>
2024-04-26 16:48:31 -0600

    Estimate material properties from radiation dose

--- pressureVessel.py
+++ pressureVessel.py
@@ -18,6 +18,7 @@ import load
 import mesh
 import job
 import visualization
+from math import log
 
 
 backwardCompatibility.setValues(includeDeprecated=True,
@@ -27,7 +28,7 @@ def printMethods(object):
     # Abaqus has terrible documentation so this function is useful for debugging
     object_methods = [method_name for method_name in dir(object)]
 
-def getElasticProperties(element, part):
+def getMaterialProperties(element, part, sourceLocation):
     # Set material properties for an element
     
     # Get location of element from nodes
@@ -49,7 +50,47 @@ def getElasticProperties(element, part):
     poissonsRatio = 0.3
     
     elasticProperties = (youngsModulus, poissonsRatio)
-    return elasticProperties
+    
+    # Estimate neutron radiation from location
+    distanceFromSource = sum([(centroid[i]-sourceLocation[i])**2 for i in range(3)])**.5  # [m]
+    Phi_reactor = 1.25e18  # [1/(cm^2*yr)]
+    Phi = Phi_reactor*1.52**2/distanceFromSource**2  # [1/(cm^2*yr)]
+    
+    t = 60  # [yr]
+    sigmaBar_d = 249.24*1e-24  # [cm^2]
+    dpa = Phi*t*sigmaBar_d  # []
+    
+    # Set material properties based on dpa
+    youngsModulus = 209E9
+    poissonsRatio = 0.3
+    if dpa==0.0:
+        sigma_y = 300e6  # [Pa]
+    else:
+        sigma_y = (52.93*log(dpa)+729.5)*1e6  # [Pa]
+    
+    epsilon_y = sigma_y/youngsModulus  # []
+    
+    # Interpolate the values from the graph in the paper to get sigma_ut, epsilon_ut
+    if dpa>3.0:
+        dpa=3.0
+    dpa_table = [0.0, 0.4, 1.25, 3.0]
+    UT_table = [500.0, 680.0, 760.0, 790.0]
+    UE_table = [.48, .1, .06, .005]
+    
+    for i in range(len(dpa_table)):
+        if dpa>=dpa_table[i] and dpa<=dpa_table[i+1]:
+            interpBin=i
+            break
+    
+    x_interp = (dpa-dpa_table[interpBin])/(dpa_table[interpBin+1]-dpa_table[interpBin])
+    sigma_ut = UT_table[interpBin]+x_interp*(UT_table[interpBin+1]-UT_table[interpBin])
+    epsilon_ut = UE_table[interpBin]+x_interp*(UE_table[interpBin+1]-UE_table[interpBin])  
+    
+    elasticProperties = (youngsModulus, poissonsRatio)
+    plasticProperties = ((sigma_y, epsilon_y-epsilon_y),(sigma_ut, epsilon_ut-epsilon_y))
+    #print("epsilon_ut: " + str(epsilon_ut))
+    
+    return elasticProperties, plasticProperties
 
 # Get model constants for AP1000
 innerRadius = 4.0386/2  # [m]
@@ -57,6 +98,8 @@ thickness = .203  # [m]
 designPressure = 17.2e6  # [Pa(a)]
 designTemperature = 343.3  # [C]
 height = 12.056  # [m]
+
+sourceLocation = [0.0, (-height/2+innerRadius)/2, 0.0]
 
 # Create a model.
 
@@ -73,7 +116,6 @@ myViewport = session.Viewport(name='Pressure Vessel',
 
 pvRotateSketch = pvModel.ConstrainedSketch(name='pvRotateSketch',
     sheetSize=100.)
-
 
 pvRotateSketch.ArcByCenterEnds(center=(0,height/2-innerRadius), 
                                point2=(0, height/2),
@@ -142,12 +184,14 @@ insideSurfaces = ((insideFace, SIDE1), (insideTopFace, SIDE1), (insideBottomFace
 pvModel.Pressure(name='Pressure', createStepName='pressureStep',
     region=insideSurfaces, magnitude=1.5e7)
 
-
 #-------------------------------------------------------
 # Create a boundary condition so it doesn't move or rotate
 bottomPoint = myInstance.vertices.findAt(((0,-height/2-thickness,0),))
-pvModel.DisplacementBC(name='PinAtBottom', createStepName = 'pressureStep', region=(bottomPoint,),
+pvModel.DisplacementBC(name='PinAtBottom', createStepName = 'Initial', region=(bottomPoint,),
                        u1=0,u2=0,u3=0,ur1=0,ur2=0,ur3=0)
+topPoint = myInstance.vertices.findAt(((0,height/2+thickness,0),))
+pvModel.DisplacementBC(name='PinAtTop', createStepName = 'Initial', region=(topPoint,),
+                       u1=0,u3=0,ur1=0,ur2=0,ur3=0)
 
 #-------------------------------------------------------
 # Assign an element type to the part instance.
@@ -160,7 +204,7 @@ pvPart.setElementType(regions=region, elemTypes=(elemType,))
 
 # Seed the part instance.
 
-pvPart.seedPart(size=1.0)
+pvPart.seedPart(size=0.75)
 
 # Mesh the part instance.
 
@@ -180,8 +224,9 @@ for e in els:
     # Create material based on element's location
     steelTemp = pvModel.Material(name='Steel'+str(e.label))
 
-    elasticProperties = getElasticProperties(e, pvPart)
+    elasticProperties, plasticProperties = getMaterialProperties(e, pvPart, sourceLocation)
     steelTemp.Elastic(table=(elasticProperties, ) )
+    steelTemp.Plastic(table=plasticProperties)
 
     # Create section with region equal to the element region
     mySection = pvModel.HomogeneousSolidSection(name='pvSection'+str(e.label),
